e60fa5bea25c1826e95e340ea6ed9f0a5a879717
Archbold <archiboldpatsanza@gmail.com>
2019-07-05 00:39:47 +0300

    home page edits

--- controllers/routingController.js
+++ controllers/routingController.js
@@ -48,5 +48,8 @@ var data=[{
       story:'But I must explain to you how all this mistaken idea of denouncing pleasure and praising pain was born and I will give you a complete account of the system, and expound the actual teachings of the great explorer of the truth, the master-builder of human happiness. No one rejects, dislikes, or avoids.........'},
  ];
     res.render('home',{data:data});
+  });
+  app.get('/calender',(req,res)=>{
+    res.send('no calender to display');
   })
 }
